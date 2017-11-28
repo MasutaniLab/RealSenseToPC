b46fe43d9aa59551d480b23f9d95031ca680e462
MASUTANI Yasuhiro <masutani@isc.osakac.ac.jp>
2017-11-28 09:25:28 +0900

    起動バッチファイルの中身を他と統一

--- RealSenseToPC.bat
+++ RealSenseToPC.bat
@@ -1,3 +1,5 @@
 @echo off
-if not defined RTM_VC_CONFIG set RTM_VC_CONFIG=Debug
-start "%NAME%Comp" "build\src\%RTM_VC_CONFIG%\%~n0.exe"
+pushd %~dp0
+if not defined RTM_VC_CONFIG set RTM_VC_CONFIG=Release
+start "%~n0" "build\src\%RTM_VC_CONFIG%\%~n0Comp.exe"
+popd
