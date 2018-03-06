1c7d1924251d6f14baa621bbc34ecdcbaca1671f
MASUTANI Yasuhiro <masutani@isc.osakac.ac.jp>
2018-03-06 12:45:56 +0900

    標準ログ機能を導入

--- src/RealSenseToPC.cpp
+++ src/RealSenseToPC.cpp
@@ -11,6 +11,7 @@
 #include <iostream>
 #include <string>
 #include <iomanip>
+#include <sstream>
 using namespace std;
 
 typedef pcl::RealSenseGrabber::TemporalFilteringType TFT;
@@ -94,7 +95,7 @@ RealSenseToPC::~RealSenseToPC()
 
 RTC::ReturnCode_t RealSenseToPC::onInitialize()
 {
-  cout << "RealSenseToPC::onInitialize()" << endl;
+  RTC_INFO(("onInitialize()"));
   // Registration: InPort/OutPort/Service
   // <rtc-template block="registration">
   // Set InPort buffers
@@ -150,7 +151,7 @@ RTC::ReturnCode_t RealSenseToPC::onShutdown(RTC::UniqueId ec_id)
 
 RTC::ReturnCode_t RealSenseToPC::onActivated(RTC::UniqueId ec_id)
 {
-  cout << "RealSenseToPC::onActivated()" << endl;
+  RTC_INFO(("onActivated()"));
   try {
     string device_id = m_device_id;
     if (device_id == " ") device_id = "";
@@ -158,15 +159,15 @@ RTC::ReturnCode_t RealSenseToPC::onActivated(RTC::UniqueId ec_id)
     vector<pcl::RealSenseGrabber::Mode> xyz_modes = m_interface->getAvailableModes(true);
     vector<pcl::RealSenseGrabber::Mode> xyzrgba_modes = m_interface->getAvailableModes(false);
     if (m_mode <= 0) {
-      cout << "Default mode" << endl;
+      RTC_INFO(("Default mode"));
     } else if (m_mode && m_mode <= xyz_modes.size()) {
-      cerr << "Depth only is not supported." << endl;
+      RTC_ERROR(("Depth only is not supported."));
       return RTC::RTC_ERROR;
     } else if (m_mode <= xyz_modes.size() + xyzrgba_modes.size()) {
-      cout << "mode: " << m_mode << endl;
+      RTC_INFO(("mode: %d", m_mode));
       m_interface->setMode(xyzrgba_modes[m_mode-xyz_modes.size ()-1], true);
     } else {
-      cerr << "mode: " << m_mode << " is not supported." << endl;
+      RTC_ERROR(("mode: %d is not supported.", m_mode));
       return RTC::RTC_ERROR;
     }
 
@@ -203,17 +204,17 @@ RTC::ReturnCode_t RealSenseToPC::onActivated(RTC::UniqueId ec_id)
     m_interface->registerCallback(f);
     m_interface->start();
     pcl::RealSenseGrabber::Mode mode = m_interface->getMode();
-#define print(x) cout << #x << ": " << x << endl
-    print(mode.fps);
-    print(mode.depth_width);
-    print(mode.depth_height);
-    print(mode.color_width);
-    print(mode.color_height);
+#define RTC_INFO_VAR(x) {stringstream ss; ss << #x << ": " << x; RTC_INFO(((ss.str().c_str())))}
+    RTC_INFO_VAR(mode.fps);
+    RTC_INFO_VAR(mode.depth_width);
+    RTC_INFO_VAR(mode.depth_height);
+    RTC_INFO_VAR(mode.color_width);
+    RTC_INFO_VAR(mode.color_height);
   } catch (pcl::io::IOException& e) {
-    cerr << "Failed to create a grabber: " << e.what () << endl; //ここでプログラムが終了してしまう問題あり
+    RTC_ERROR(("Failed to create a grabber: %s", e.what())); //ここでプログラムが終了してしまう問題あり
     return RTC::RTC_ERROR;
   } catch (...) {
-    cerr << "An exception occurred while starting grabber" << endl;
+    RTC_ERROR(("An exception occurred while starting grabber"));
     return RTC::RTC_ERROR;
   }
 
@@ -223,7 +224,7 @@ RTC::ReturnCode_t RealSenseToPC::onActivated(RTC::UniqueId ec_id)
 
 RTC::ReturnCode_t RealSenseToPC::onDeactivated(RTC::UniqueId ec_id)
 {
-  cout << "RealSenseToPC::onDeactivated()" << endl;
+  RTC_INFO(("onDeactivated()"));
   m_interface->stop();
   m_interface = nullptr;
 
@@ -238,7 +239,7 @@ RTC::ReturnCode_t RealSenseToPC::onExecute(RTC::UniqueId ec_id)
     if (m_window_prev < 1) {
       m_window_prev = 1;
     }
-    cout << "Temporal filtering window size: " << m_window_prev << endl;
+    RTC_INFO(("Temporal filtering window size: %d", m_window_prev));
     m_interface->enableTemporalFiltering(TFT(m_temporal_filtering_prev), m_window_prev);
   }
 
@@ -250,22 +251,21 @@ RTC::ReturnCode_t RealSenseToPC::onExecute(RTC::UniqueId ec_id)
     if (m_threshold_prev > 15) {
       m_threshold_prev = 15;
     }
-    cout << "Confidence threshold: " << m_threshold_prev << endl;
+    RTC_INFO(("Confidence threshold: %d", m_threshold_prev));
     m_interface->setConfidenceThreshold(m_threshold_prev);
   }
 
   if (m_temporal_filtering != m_temporal_filtering_prev) {
     m_temporal_filtering_prev = m_temporal_filtering;
-    cout << "Temporal filtering: ";
     switch (m_temporal_filtering_prev) {
     case pcl::RealSenseGrabber::RealSense_None:
-      cout <<"none" << endl;
+      RTC_INFO(("Temporal filtering: none"));;
       break;
     case pcl::RealSenseGrabber::RealSense_Median:
-      cout << "median" << endl;
+      RTC_INFO(("Temporal filtering: median"));;
       break;
     case pcl::RealSenseGrabber::RealSense_Average:
-      cout << "average" << endl;
+      RTC_INFO(("Temporal filtering: average"));;
       break;
     }
     m_interface->enableTemporalFiltering(TFT(m_temporal_filtering_prev), m_window_prev);
@@ -273,7 +273,7 @@ RTC::ReturnCode_t RealSenseToPC::onExecute(RTC::UniqueId ec_id)
 
   if (m_with_bilateral != m_with_bilateral_prev) {
     m_with_bilateral_prev = m_with_bilateral;
-    cout << "Bilateral filtering: " << (m_with_bilateral_prev ? "ON" : "OFF") << endl;
+    RTC_INFO(("Bilateral filtering: %s", (m_with_bilateral_prev ? "ON" : "OFF")));;
   }
 
   if (m_bilateral_sigma_s != m_bilateral_sigma_s_prev) {
@@ -281,7 +281,7 @@ RTC::ReturnCode_t RealSenseToPC::onExecute(RTC::UniqueId ec_id)
     if (m_bilateral_sigma_s_prev < 1) {
       m_bilateral_sigma_s_prev = 1;
     }
-    cout << "Bilateral filter spatial sigma: " << m_bilateral_sigma_s_prev << endl;
+    RTC_INFO(("Bilateral filter spatial sigma: %d", m_bilateral_sigma_s_prev));
     m_bilateral.setSigmaS (m_bilateral_sigma_s_prev);
   }
 
@@ -290,7 +290,7 @@ RTC::ReturnCode_t RealSenseToPC::onExecute(RTC::UniqueId ec_id)
     if (m_bilateral_sigma_r_prev < 0.01) {
       m_bilateral_sigma_r_prev = 0.01;
     }
-    cout << "Bilateral filter range sigma: " << m_bilateral_sigma_r_prev << endl;
+    RTC_INFO(("Bilateral filter range sigma: %f", m_bilateral_sigma_r_prev));
     m_bilateral.setSigmaR(m_bilateral_sigma_r_prev);
   }
 
@@ -372,7 +372,7 @@ void RealSenseToPC::cloud_cb(const pcl::PointCloud<PointT>::ConstPtr &cloudOrg)
   {
     double now = pcl::getTime();
 #if 1
-    cout << "Average framerate: " << double(count)/double(now - last) << " Hz" <<  endl;
+    RTC_INFO(("Average framerate: %lf Hz", double(count)/double(now - last)));
 #endif
     count = 0;
     last = now;
@@ -383,45 +383,44 @@ void RealSenseToPC::printDeviceList()
 {
   typedef boost::shared_ptr<pcl::RealSenseGrabber> RealSenseGrabberPtr;
   std::vector<RealSenseGrabberPtr> grabbers;
-  std::cout << "Connected devices: ";
-  boost::format fmt ("\n  #%i  %s");
-  boost::format fmt_dm ("\n        %2i) %d Hz  %dx%d Depth");
-  boost::format fmt_dcm ("\n        %2i) %d Hz  %dx%d Depth  %dx%d Color");
+  RTC_INFO(("Connected devices:"));
+  boost::format fmt ("  #%i  %s");
+  boost::format fmt_dm ("        %2i) %d Hz  %dx%d Depth");
+  boost::format fmt_dcm ("        %2i) %d Hz  %dx%d Depth  %dx%d Color");
   while (true)
   {
     try
     {
       grabbers.push_back (RealSenseGrabberPtr (new pcl::RealSenseGrabber));
-      std::cout << boost::str (fmt % grabbers.size () % grabbers.back ()->getDeviceSerialNumber ());
+      RTC_INFO((boost::str (fmt % grabbers.size () % grabbers.back ()->getDeviceSerialNumber ()).c_str()));
       std::vector<pcl::RealSenseGrabber::Mode> xyz_modes = grabbers.back ()->getAvailableModes (true);
-      std::cout << "\n      Depth modes:";
-      if (xyz_modes.size ())
-        for (size_t i = 0; i < xyz_modes.size (); ++i)
-          std::cout << boost::str (fmt_dm % (i + 1) % xyz_modes[i].fps % xyz_modes[i].depth_width % xyz_modes[i].depth_height);
-      else
-      {
-        std::cout << " none";
+      RTC_INFO(("      Depth modes:"));
+      if (xyz_modes.size()) {
+        for (size_t i = 0; i < xyz_modes.size(); ++i) {
+          RTC_INFO((boost::str(fmt_dm % (i + 1) % xyz_modes[i].fps % xyz_modes[i].depth_width % xyz_modes[i].depth_height).c_str()));
+        }
+      } else {
+        RTC_INFO((" none"));
       }
       std::vector<pcl::RealSenseGrabber::Mode> xyzrgba_modes = grabbers.back ()->getAvailableModes (false);
-      std::cout << "\n      Depth + color modes:";
+      RTC_INFO(("      Depth + color modes:"));
       if (xyz_modes.size ())
         for (size_t i = 0; i < xyzrgba_modes.size (); ++i)
         {
           const pcl::RealSenseGrabber::Mode& m = xyzrgba_modes[i];
-          std::cout << boost::str (fmt_dcm % (i + xyz_modes.size () + 1) % m.fps % m.depth_width % m.depth_height % m.color_width % m.color_height);
+          RTC_INFO((boost::str(fmt_dcm % (i + xyz_modes.size () + 1) % m.fps % m.depth_width % m.depth_height % m.color_width % m.color_height).c_str()));
         }
       else
-        std::cout << " none";
+        RTC_INFO((" none"));
     }
     catch (pcl::io::IOException& e)
     {
       break;
     }
   }
-  if (grabbers.size ())
-    std::cout << std::endl;
-  else
-    std::cout << "none" << std::endl;
+  if (grabbers.size()==0) {
+    RTC_INFO(("none"));
+  }
 }
 
 extern "C"
